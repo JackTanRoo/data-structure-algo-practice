c25e63e9a37953f234cd19261165b0e96a15763d
JackTanRoo <jack.tan.lu@hotmail.com>
2019-06-30 23:15:55 -0700

    big 0 notation continued

--- BigO.js
+++ BigO.js
@@ -15,4 +15,15 @@ function addUpTo(n) {
 var t1 = performance.now();
 addUpTo(1000000000);
 var t2 = performance.now();
-console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+console.log(`Time Elapsed for 1st: ${(t2 - t1) / 1000} seconds.`)
+
+
+
+function addUpTo2(n) {
+  return n * (n + 1) / 2;
+}
+
+var time1 = performance.now();
+addUpTo2(1000000000);
+var time2 = performance.now();
+console.log(`Time Elapsed for 2nd: ${(time2 - time1) / 1000} seconds.`)
