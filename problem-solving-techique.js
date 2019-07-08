5acee515852ca9254862f7d0cc6100e101acff75
JackTanRoo <jack.tan.lu@hotmail.com>
2019-07-07 21:15:24 -0700

    solution patterns

---
+++ problem-solving-techique.js
@@ -0,0 +1,48 @@
+// 1 clarify the problem
+
+1 - what is the gioal
+2 - can i restate the problem in my own words
+3 - what should be the inputs
+4 - what should be the outputs
+5 - how can i label the data
+
+// 2 explore concrete examples
+
+help provide sanity checks
+help you understand the problem better
+
+start with some simple examples with simple inputs and outputs
+then
+- simple
+- complex
+- empty outputs
+- outlier / invalid outputs
+
+// 3 break it down
+
+Write the high level detail of your solution
+
+// 4 Solve the problem or simplify it
+
+	// dont get stuck on a difficult part of the problem
+
+	// if you think there is some hard part that you don't know how to start
+
+	// ignore the hard part first
+
+	// start writing the code for the simple part
+
+	// then incorporate the difficult part later on
+
+// 5 Look back and refactor the code
+
+	// This is the most important, dont just stop after your code works
+
+	// Questions
+	// 	Can you derive the result differently using another approach
+	// 	Is it understandable at a glance
+	// 	Can you use the method for other problem?
+	// 	Can you improve the performance?
+	// 	Can you think of ways to refactor?
+	// 	How have others solved this problem?
+
