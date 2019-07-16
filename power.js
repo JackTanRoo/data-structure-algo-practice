77cdab8a2d099bf950e0b34315a92d0da42eba5e
JackTanRoo <jack.tan.lu@hotmail.com>
2019-07-15 23:45:36 -0700

    power recursion

---
+++ power.js
@@ -0,0 +1,11 @@
+console.log(power(2,0)) // 1
+console.log(power(2,2)) // 4
+console.log(power(2,4)) // 16
+
+function power(base, exponent) {
+	if (exponent === 0) {
+		return base * 1
+	}
+
+	return base * power(base, exponent--)
+}
