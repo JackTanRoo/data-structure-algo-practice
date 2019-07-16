50c94745743a819f80f3f2ee7fcda46fd1a5734a
JackTanRoo <jack.tan.lu@hotmail.com>
2019-07-15 23:48:40 -0700

    finished power

--- power.js
+++ power.js
@@ -4,8 +4,8 @@ console.log(power(2,4)) // 16
 
 function power(base, exponent) {
 	if (exponent === 0) {
-		return base * 1
-	}
+		return 1
+	} 
 
-	return base * power(base, exponent--)
+	return base * power(base, exponent-1)
 }
