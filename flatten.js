6ecaa97d5b54bfd2158753d3d8711cb63a3f2db2
JackTanRoo <jack.tan.lu@hotmail.com>
2019-07-18 21:21:28 -0700

    did most of flatten

---
+++ flatten.js
@@ -0,0 +1,37 @@
+
+var result = []
+
+function flatten(){
+  // add whatever parameters you deem necessary - good luck!
+
+  // create a result array
+
+  // concat that result array as we circle through each element of the array
+
+  // when element is an array with length 1 or a single element, then return that element
+  var inputs =  Array.prototype.slice.call(arguments)[0];
+
+  // console.log("first", inputs)
+
+  if (!Array.isArray(inputs[0]) && inputs.length === 1) {
+  	return inputs[0]
+  }
+
+  if (Array.isArray(inputs[0])) {
+  	return flatten(inputs[0])
+  }
+  
+
+  var firstElement = inputs.shift();
+  
+  // console.log("second", inputs)
+  
+  return result.concat(firstElement, flatten(inputs))
+
+
+}
+
+console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
+console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
+console.log(flatten([[1],[2],[3]])) // [1,2,3]
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3
