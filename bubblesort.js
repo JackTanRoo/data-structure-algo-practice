289bc9d44c03c29dddcd8eb3573fb4976fb1020e
JackTanRoo <jack.tan.lu@hotmail.com>
2019-07-21 16:20:50 -0700

    restarting bubble sort

---
+++ bubblesort.js
@@ -0,0 +1,15 @@
+// bubble sort
+
+// takes an array and outputs sorted array
+
+// iterates through array
+
+// swaps the bigger number with smaller number
+
+// after first iteration, second iteration is to iterate through the full loop except last element, and shorten
+
+// for already sorted arrays, check if there is 
+
+
+
+bubbleSort([8,1,2,3,4,5,6,7]); // 1,2,3,4,5,6,7
