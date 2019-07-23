8de04a2d31ea8b6af0082f954a798f89e20ee277
JackTanRoo <jack.tan.lu@hotmail.com>
2019-07-22 20:46:06 -0700

    mergeSort implemented

--- mergeSort.js
+++ mergeSort.js
@@ -24,4 +24,31 @@
 
 merge([100,200], [1,2,3,5,6])
 
-function 
+function merge(array1, array2) {
+	var result = []
+
+
+	// define a pointer for array 1 and a pointer for array 2
+
+
+	// find the shorter array
+
+
+	// whilest pointerShort < length of shorter array
+
+
+		// if arrayShort[pointerShorter] <= arrayLong[pointerLonger]
+
+		// push arrayShort element to result + iterate pointer Shorter up by 1
+
+		// else push arrayLong element to result + iterate pointer Longer up by 1
+
+	// loop over LongerArray from pointerLong to end of LongerArray
+
+		// push the remaining elments into the result array
+
+
+
+	return result;
+
+}
