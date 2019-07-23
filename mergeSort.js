cf757f8a691bd79ee7ab2ddd1719c43ca3f7e938
JackTanRoo <jack.tan.lu@hotmail.com>
2019-07-22 20:37:24 -0700

    done merge sort

---
+++ mergeSort.js
@@ -0,0 +1,27 @@
+// merge sort
+
+// split unsorted array until you have individual element in its own array of 1 element
+
+// then join the the sorted individual arrays into arrays of 2 but in sorted order
+
+// then merge the arrays of 2 into sorted arrays of 4
+
+
+// key splits
+
+// split into 1s
+
+// sort
+
+// merge sorted arrays into larger arrays
+
+
+
+// STEP 1
+
+// implement merge - which takes 2 sorted arrays, then creates joint result array of sorted elements
+
+
+merge([100,200], [1,2,3,5,6])
+
+function 
