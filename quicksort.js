971fc037195729a1f399b9db580117a07a231186
JackTanRoo <jack.tan.lu@hotmail.com>
2019-07-27 15:32:45 -0700

    quick sort implementation

---
+++ quicksort.js
@@ -0,0 +1,13 @@
+// // designate an element as a pivot
+
+// all elements that is smaller than, move to the left of privot
+
+// same as right
+
+// but order on left and right does not matter
+
+// very space efficient, all elements are moved in place, e.g no new array is created
+
+// then recursively do the same thing on the left side
+
+// then recursively do the right side
