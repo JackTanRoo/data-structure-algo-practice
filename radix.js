d0480c4e75ee91c34b68c1f56aa69fed1899b29f
JackTanRoo <jack.tan.lu@hotmail.com>
2019-07-28 19:08:03 -0700

    radix sort helper implemented

--- radix.js
+++ radix.js
@@ -12,3 +12,19 @@
 
 // then do it until the number with highest number of digits
 
+
+
+// FIRST HELPER FUNCTION
+
+// GET DIGIT
+
+// returns the actual digit in num at a given place value
+
+// getDigit (num, place)
+
+function getDigit (num, i) {
+	return Math.floor(Math.abs(num) / Math.pow(10,i)) % 10;
+}
+
+// for 7323
+// = 7323 / 100 = 73.23 = 73 (because of Math.floor) = 3 (remainder after dividing by 10)
