21d408bd3630d2f447748ae95b067649daca9c12
JackTanRoo <jack.tan.lu@hotmail.com>
2019-07-28 19:02:07 -0700

    radix sort

---
+++ radix.js
@@ -0,0 +1,14 @@
+// compares the digits of the number into buckets of those digits
+
+// start with last digit in the number
+
+// then arraynge the numbers based on last digit
+
+// then go to the second last digit
+
+// classify to the bucket again based on new order of the nunmbers
+
+//  then rearrange again
+
+// then do it until the number with highest number of digits
+
