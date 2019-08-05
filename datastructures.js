50b1aa68cf1702eff7cbe49864724bde8c5606a0
JackTanRoo <jack.tan.lu@hotmail.com>
2019-08-04 19:52:41 -0700

    instance methods

--- datastructures.js
+++ datastructures.js
@@ -16,18 +16,30 @@
 
 
 class Student {
-	constructor (firstName, lastName, year) {
+	constructor (firstName, lastName, grade) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.grade = grade
+	}
+
+	// instance method - acts on the instance of the method only
+	fullName () {
+		return 'Your full name is ' + this.firstName + " " + this.lastName
 	}
 }
 
 let firstStudent = new Student ("jack", "lu", "8")
 
 console.log(firstStudent)
+console.log(firstStudent.fullName())
 
 // in this case
 
 // this refers to the current individual instance of the class
 
+
+// instance methods - only apply to the particular instance of the class
+
+// e.g. when array.push(xxx); push acts on the single instances
+
+// does not work on the class level
