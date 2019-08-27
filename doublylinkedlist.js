a41a99ea0175bfd5b1e4e70882e070c1eb184699
JackTanRoo <jack.tan.lu@hotmail.com>
2019-08-26 22:18:56 -0700

    started double link list

--- doublylinkedlist.js
+++ doublylinkedlist.js
@@ -2,3 +2,19 @@
 // except every node has a operation to point to previous node
 // more flexibility
 // but more memory
+
+
+class Node {
+	constructor (val){
+		this.val = val;
+		this.next = null
+	}
+}
+
+class SinglyLinkedList {
+	constructor(){
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+}
