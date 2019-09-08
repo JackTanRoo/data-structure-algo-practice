69a753ec9d22dcacd352010a370908b53744da46
JackTanRoo <jack.tan.lu@hotmail.com>
2019-09-07 17:55:59 -0700

    binary search tree done

---
+++ binarysearchtrees.js
@@ -0,0 +1,24 @@
+// tree is a new type of data structure
+
+// data structure that consists fo ndoes in a parent/child relationship
+
+// lists are linear but trees are non linear eg tree has multiple paths
+
+// singly linked list is like a linear version of a tree
+
+// but a tree must only have parent child relationships for nodes. child nodes cannot point to parent nodes or ndoes at the parent level, that is not a tree it is a graph
+
+// tree only have 1 root
+
+// applicaitons for trees
+	// HTML this is a tree
+	// 
+
+
+// binary search trees
+
+// excel at searching and retrieval of data
+
+// rules about BST
+// each node only has 2 children at most, you can also only have 1
+// sorted - left side is smaller than the parent, right is bigger
