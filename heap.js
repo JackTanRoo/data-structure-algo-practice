47e1221410d84587b418d2b1fe2ee95d4cb94872
JackTanRoo <jack.tan.lu@hotmail.com>
2019-09-15 23:11:06 -0700

    heaps

---
+++ heap.js
@@ -0,0 +1,7 @@
+// heap - max heap, where the parent is always larger than a heap
+// no guaratnees between the siblings
+// every left and right are filled 
+// left is filled out first
+// no ordering, so a child for a peer parent can be bigger than a peer sibling of their parent
+
+// vice versa for min heap
